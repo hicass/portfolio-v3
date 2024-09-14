19253c6cd7f526d11d72061403b7333c7673af2f
hicass <cassidyjwalters@gmail.com>
2024-09-13 17:08:57 -0700

    Create an about component that maps through the about section data to display the paragraphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 import Hero from './components/Hero/Hero';
+import About from './components/About';
 
 const LandingPage: FC = () => {
   return (
     <main>
-      <div className='overflow-y-visible overflow-x-hidden h-screen relative parallax-wrapper'>
+      <div className='flex flex-col items-center overflow-y-visible overflow-x-hidden h-screen relative font-[family-name:var(--font-satoshi)] parallax-wrapper'>
         <Hero />
+
+        <div className='flex flex-col gap-10 w-2/3 mt-10'>
+          <About />
+        </div>
       </div>
     </main>
   );
