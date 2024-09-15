3f75d4669651fc4a1c19fa8193392d8841713824
hicass <cassidyjwalters@gmail.com>
2024-09-14 19:23:13 -0700

    Create a Nav component that has two groups, a nav group that has links to different parts of the page, and an aside group that is opposite to the nav that has links to my social media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import Hero from './components/Hero/Hero';
 import About from './components/About';
 import Tech from './components/Tech';
+import Nav from './components/Nav';
 
 const LandingPage: FC = () => {
   return (
     <main>
-      <div className="flex flex-col items-center overflow-y-visible overflow-x-hidden h-screen relative font-[family-name:var(--font-satoshi)] parallax-wrapper">
+      <div className="flex flex-col items-center overflow-y-visible overflow-x-hidden h-screen w-screen relative font-[family-name:var(--font-satoshi)] parallax-wrapper">
         <Hero />
+        <Nav/>
 
         <div className="flex flex-col gap-48 w-2/3 mt-10">
           <About />
