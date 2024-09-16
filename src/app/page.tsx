72a24bb771014aa97425d41b42ec448c787a15cc
hicass <cassidyjwalters@gmail.com>
2024-09-16 12:47:44 -0700

    Fix bug that makes page links un-clickable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,15 @@ import Nav from './components/Nav';
 const LandingPage: FC = () => {
   return (
     <main>
-      <div className="flex flex-col items-center overflow-y-visible overflow-x-hidden h-screen w-screen relative font-[family-name:var(--font-satoshi)] parallax-wrapper">
+      <div className="flex flex-col items-start overflow-y-visible overflow-x-hidden h-screen w-screen relative font-[family-name:var(--font-satoshi)] parallax-wrapper">
         <Hero />
-        <Nav/>
+        <Nav />
 
-        <div className="flex flex-col gap-48 w-2/3 mt-10">
-          <About />
-          <Tech />
+        <div className="w-full flex flex-col items-center mt-10">
+          <div className="w-2/3 flex flex-col gap-48">
+            <About />
+            <Tech />
+          </div>
         </div>
       </div>
     </main>
