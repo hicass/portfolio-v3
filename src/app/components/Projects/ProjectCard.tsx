b46b070f80037003c6cde6864ef7db7e1a067966
hicass <hicass.dev@gmail.com>
2025-01-24 13:23:15 -0800

    Add an arrow that moves up and turns orange when the card is hovered over

--- src/app/components/Projects/ProjectCard.tsx
+++ src/app/components/Projects/ProjectCard.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { motion, Variants } from 'framer-motion';
 
 import Image from 'next/image';
+import { MdOutlineArrowOutward } from "react-icons/md";
 
 type Project = {
   title: string;
@@ -41,7 +42,7 @@ const cardVariants: Variants = {
 
 const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
   return (
-    <a href={project.liveLink} target="_blank" className="hover:cursor-pointer">
+    <a href={project.liveLink} target="_blank" className="group transition hover:cursor-pointer">
       <motion.article
         variants={cardVariants}
         initial="offscreen"
@@ -50,7 +51,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
           once: true,
           amount: 0.1,
         }}
-        className="flex flex-col w-full gap-6 p-4 rounded-lg border-t-[1px] md:border-t-transparent border-t-green bg-dark-green-1 md:hover:border-t-green md:bg-transparent md:hover:bg-dark-green-1 z-40"
+        className="flex flex-col w-full gap-4 p-4 rounded-lg border-t-[1px] md:border-t-transparent border-t-green bg-dark-green-1 md:hover:border-t-green md:bg-transparent md:hover:bg-dark-green-1 z-40"
       >
         {/* Top Section */}
         <div className="flex flex-col w-full lg:flex-row gap-6">
@@ -67,8 +68,9 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
 
           {/* Information about the project */}
           <div className="flex flex-col gap-4 lg:w-1/2">
-            <header className="flex justify-between">
+            <header className="flex justify-between group-hover:text-orange">
               <h4 className="text-2xl">{project.title}</h4>
+              <MdOutlineArrowOutward className='transform transition-transform duration-300 group-hover:translate-x-2 group-hover:-translate-y-2'/>
             </header>
 
             <p className="text-lg">{project.description}</p>
