d451e34ee5c948cb3f005a2c02e44f767c1b0656
hicass <hicass.dev@gmail.com>
2025-06-16 11:38:14 -0700

    Add revalidation to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { client } from '@/sanity/lib/client';
 import MainPageContent from './components/MainPageContent';
 
+export const revalidate = 30;
+
 export default async function MainPage() {
   const AboutData = await client.fetch(`*[_type == "about"][0]`);
 
